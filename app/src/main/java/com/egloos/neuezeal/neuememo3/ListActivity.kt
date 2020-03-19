70b2e4eda61d0b24d904c84fbe1d34d410fb229e
leeyunhome <leeyunhome@daum.net>
2020-03-19 23:22:08 +0900

    Initial commit

---
+++ app/src/main/java/com/egloos/neuezeal/neuememo3/ListActivity.kt
@@ -0,0 +1,23 @@
+package com.egloos.neuezeal.neuememo3
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_list.*
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            val intent = Intent(applicationContext, DetailActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/egloos/neuezeal/neuememo3/MemoListFragment.kt
@@ -0,0 +1,22 @@
+package com.egloos.neuezeal.neuememo3
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MemoListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_memo_list, container, false)
+    }
+
+}
